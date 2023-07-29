3a35bf01f7b98293fa0538c0418eace8b92c0224
JH <764825656@qq.com>
2023-07-29 22:33:13 +0100

    custom error middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,7 @@ import Product from '../models/productModel.js';
 
 router.get('/', asyncHandler(async (req,res) => {
   const products = await Product.find({});
+  throw new Error('Some error');
   res.json(products);
 }));
 
@@ -13,8 +14,10 @@ router.get('/:id',asyncHandler(async(req,res)=>{
 
   if (product) {
     return res.json(product);
+  }else{
+  res.status(404);
+  throw new Error('Resource not found');
   }
-  res.status(404).json({ message: 'Product not found'});
 })
 );
 export default router;
